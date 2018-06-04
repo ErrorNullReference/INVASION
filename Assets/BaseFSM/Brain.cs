0e3f70ca848dbf599d1d3aa010da4b36f52eb9f0
SamueleAlpino <samuelealpino@gmail.com>
2018-06-04 19:14:58 +0200

    FSM implemented

---
+++ Assets/BaseFSM/Brain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class AISwitchEvent : UnityEvent<AIBehaviour> { }
+
+public class Brain : MonoBehaviour
+{
+    public AIBehaviour CurrentState { get { return currentState; } }
+    public AIBehaviour PreviousState
+    {
+        get
+        {
+            if (previousState != null)
+                return previousState;
+            else
+                return currentState;
+        }
+    }
+
+    [SerializeField]
+    private AIBehaviour currentState;
+    private AIBehaviour previousState;
+
+    private void Start()
+    {
+        if (currentState != null)
+        {
+            currentState.Init(this);
+            currentState.OnStateEnter();
+        }
+
+
+    }
+
+    private void Update()
+    {
+        if (currentState != null)
+            currentState.AIUpdate();
+    }
+
+    public void SwitchState(AIBehaviour next)
+    {
+        currentState.OnStateExit();
+
+        previousState = this.currentState;
+        currentState = next;
+        if (!currentState.Initialized)
+            currentState.Init(this);
+
+        currentState.OnStateEnter();
+    }
+}
