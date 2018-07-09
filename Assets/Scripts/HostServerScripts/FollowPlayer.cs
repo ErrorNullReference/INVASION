3323c9538052d8958c022ca96526235259d0fd6a
aannun <alex.aaav@hotmail.it>
2018-07-09 11:19:35 +0200

    Added Develop Test Scene

--- Assets/Scripts/HostServerScripts/FollowPlayer.cs
+++ Assets/Scripts/HostServerScripts/FollowPlayer.cs
@@ -3,25 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using SOPRO;
 using System;
+
 public class FollowPlayer : MonoBehaviour
 {
     public SOListPlayerContainer Players;
     public ReferenceVector3 Offset;
+    public bool Debug;
 
     [NonSerialized]
     public int CurrentIndex = -1;
+
     public void SetFollowLocalPlayer()
     {
-        for (int i = 0; i < Players.Elements.Count; i++)
+        if (!Debug)
         {
-            Player p = Players[i];
-            if (!p.Dead && p.Avatar.UserInfo.SteamID == Client.MyID)
+            for (int i = 0; i < Players.Elements.Count; i++)
             {
-                CurrentIndex = i;
-                break;
+                Player p = Players[i];
+                if (!p.Dead && p.Avatar.UserInfo.SteamID == Client.MyID)
+                {
+                    CurrentIndex = i;
+                    break;
+                }
             }
         }
+        else
+        {
+            CurrentIndex = 0;
+        }
     }
+
     private void Start()
     {
         SetFollowLocalPlayer();
