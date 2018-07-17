3ed27ff7a39ecff255972b89de1932512030d978
aannun <alex.aaav@hotmail.it>
2018-07-17 12:00:01 +0200

    Fixed Dll error. Fixed enemy mgr

--- Assets/Scripts/EnemySpawnScripts/HostEnemySpawner.cs
+++ Assets/Scripts/EnemySpawnScripts/HostEnemySpawner.cs
@@ -60,7 +60,7 @@ public class HostEnemySpawner : MonoBehaviour
         if (EnemySpawnGraph.GetSpawn(out NumEnemiesToSpawn, Time.deltaTime))
         {
             for (int i = 0; i < NumEnemiesToSpawn; i++)
-                InstantiateEnemy(EnemyType.Normal, NearestSpawnPointOutsideView);
+                InstantiateEnemy(EnemyType.Normal, AllSpawnPointsOutsideView.Elements[Random.Range(0, AllSpawnPointsOutsideView.Elements.Count)]);
         }
     }
 
