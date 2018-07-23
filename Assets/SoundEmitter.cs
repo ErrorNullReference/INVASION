e2770907463feb1c0b804807c02970d719bf1c8a
oWASDo <sem961@gmail.com>
2018-07-23 09:00:49 +0200

    Audio modification

---
+++ Assets/SoundEmitter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundEmitter : MonoBehaviour {
+
+    private AudioSource soundSource;
+
+    private void Awake()
+    {
+        soundSource = GetComponent<AudioSource>();
+    }
+
+    public void EmitSound()
+    {
+        soundSource.Play();
+    }
+    public void EmitSoundOneShot()
+    {
+        soundSource.PlayOneShot(soundSource.clip);
+    }
+}
