6420cc35fa3411588b4d40736ed1661bfa32071f
aannun <alex.aaav@hotmail.it>
2018-08-04 20:42:28 +0200

    Added rotating light

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public Vector3 Direction;
+    public float Speed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Direction * Speed * Time.deltaTime);
+    }
+}
