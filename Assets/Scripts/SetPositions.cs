6d326ad443fae958cf8fe1e548c796a4bb5ac2d8
aannun <alex.aaav@hotmail.it>
2018-08-08 12:28:14 +0200

    Fixed spawn points positions

--- Assets/Scripts/SetPositions.cs
+++ Assets/Scripts/SetPositions.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using SOPRO;
+
 public class SetPositions : MonoBehaviour
 {
     public SOListVector3Container PositionsToFill;
-    public bool ClearFirst = true;
+    public bool ClearFirst = true, DrawGizmos = true;
+
     void Start()
     {
         if (ClearFirst)
@@ -19,4 +21,16 @@ public class SetPositions : MonoBehaviour
         Destroy(this);
     }
 
+    void OnDrawGizmos()
+    {
+        if (!DrawGizmos)
+            return;
+        
+        Gizmos.color = Color.red;
+        Transform[] childs = GetComponentsInChildren<Transform>();
+        for (int i = 1; i < childs.Length; i++)
+        {
+            Gizmos.DrawSphere(childs[i].position, 1);
+        }
+    }
 }
