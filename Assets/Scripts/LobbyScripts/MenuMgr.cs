1b609a8a29089dc2cb89479316936327bad562a7
Alessio Annunziato <alessio.annunz@gmail.com>
2018-08-14 13:18:24 +0200

    Friend Invite with steam overlay

--- Assets/Scripts/LobbyScripts/MenuMgr.cs
+++ Assets/Scripts/LobbyScripts/MenuMgr.cs
@@ -5,52 +5,51 @@ using Steamworks;
 
 public class MenuMgr : MonoBehaviour
 {
-    public Canvas EnterLobby, AvatarSelection, FriendsList, Chat;
+	public Canvas EnterLobby, AvatarSelection, FriendsList, Chat;
 
-    // Use this for initialization
-    void Start()
-    {
-        EnterLobby.gameObject.SetActive(true);
+	// Use this for initialization
+	void Start ()
+	{
+		EnterLobby.gameObject.SetActive (true);
 
-        SteamCallbackReceiver.LobbyEnterEvent += ChangeLobbyState;
-        SteamCallbackReceiver.LobbyCreateEvent += ChangeLobbyState;
-    }
+		SteamCallbackReceiver.LobbyEnterEvent += ChangeLobbyState;
+		SteamCallbackReceiver.LobbyCreateEvent += ChangeLobbyState;
+	}
 
-    void ChangeLobbyState(LobbyEnter_t cb)
-    {
-        if (cb.m_ulSteamIDLobby != 0)
-        {
-            EnterLobby.gameObject.SetActive(false);
-            AvatarSelection.gameObject.SetActive(true);
-            Chat.gameObject.SetActive(true);
-        }
-    }
+	void ChangeLobbyState (LobbyEnter_t cb)
+	{
+		if (cb.m_ulSteamIDLobby != 0) {
+			EnterLobby.gameObject.SetActive (false);
+			AvatarSelection.gameObject.SetActive (true);
+			Chat.gameObject.SetActive (true);
+		}
+	}
 
-    void ChangeLobbyState(LobbyCreated_t cb)
-    {
-        if (cb.m_ulSteamIDLobby != 0)
-        {
-            EnterLobby.gameObject.SetActive(false);
-            AvatarSelection.gameObject.SetActive(true);
-            Chat.gameObject.SetActive(true);
-        }
-    }
+	void ChangeLobbyState (LobbyCreated_t cb)
+	{
+		if (cb.m_ulSteamIDLobby != 0) {
+			EnterLobby.gameObject.SetActive (false);
+			AvatarSelection.gameObject.SetActive (true);
+			Chat.gameObject.SetActive (true);
+		}
+	}
 
-    void OnDestroy()
-    {
-        SteamCallbackReceiver.LobbyEnterEvent -= ChangeLobbyState;
-        SteamCallbackReceiver.LobbyCreateEvent -= ChangeLobbyState;
-    }
+	void OnDestroy ()
+	{
+		SteamCallbackReceiver.LobbyEnterEvent -= ChangeLobbyState;
+		SteamCallbackReceiver.LobbyCreateEvent -= ChangeLobbyState;
+	}
 
-    public void OpenFriendList()
-    {
-        FriendsList.gameObject.SetActive(true);
-    }
+	public void OpenFriendList ()
+	{
+		//FriendsList.gameObject.SetActive(true);
+		SteamFriends.ActivateGameOverlayInviteDialog (Client.Lobby.LobbyID);
+	}
 
-    public void ReturnToSelection()
-    {
-        EnterLobby.gameObject.SetActive(true);
-        AvatarSelection.gameObject.SetActive(false);
-        Chat.gameObject.SetActive(false);
-    }
+	public void ReturnToSelection ()
+	{
+		EnterLobby.gameObject.SetActive (true);
+		AvatarSelection.gameObject.SetActive (false);
+		Chat.gameObject.SetActive (false);
+	}
 }
