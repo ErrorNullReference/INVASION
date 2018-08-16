a5805175adbfe2590b3e0a7066bc5e1b6be33941
Alessio Annunziato <alessio.annunz@gmail.com>
2018-08-16 10:35:17 +0200

    Copied finalScene map to playScene. Updated AI.

--- Assets/Scripts/BaseFSM/AIShootTillOnSight.cs
+++ Assets/Scripts/BaseFSM/AIShootTillOnSight.cs
@@ -2,116 +2,199 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using SOPRO;
 
 public class AIShootTillOnSight : AIBehaviour
 {
-    [SerializeField]
-    private LayerMaskHolder targetLayerMask;
-    [SerializeField]
-    private float maxViewDistance;
+	[SerializeField]
+	private LayerMaskHolder targetLayerMask;
+	[SerializeField]
+	private float maxViewDistance;
+	[SerializeField]
+	private float midDistance;
+	[SerializeField]
+	private float minDistance;
+	[SerializeField]
+	[Range (1, 360)]
+	private int fov;
+	[SerializeField]
+	private float minCooldownBetweenShoots;
+	[SerializeField]
+	private float maxCooldownBetweenShoots;
+	private float currentCooldownBetweenShoots;
+	[SerializeField]
+	private float cooldownBeforeRecalculation;
+	private float currentCooldownLeftBeforeRecalculation;
 
-    [SerializeField]
-    private float minCooldownBetweenShoots;
-    [SerializeField]
-    private float maxCooldownBetweenShoots;
-    private float currentCooldownBetweenShoots;
+	[SerializeField]
+	private Transform muzzle;
+	private Player targetToShot;
 
-    [SerializeField]
-    private Transform muzzle;
-    private Player targetToShot;
+	public EnemyShootSync shootSync;
+	Gun gun;
 
-    public EnemyShootSync shootSync;
-    Gun gun;
+	[SerializeField]
+	private AIBehaviour targetLost;
+	[SerializeField]
+	private AIBehaviour goToPlayer;
 
-    [SerializeField]
-    private AIBehaviour targetLost;
+	private NavMeshHit navMeshHit;
+	private NavMeshAgent agent;
+	private AnimationControllerScript animController;
 
-    protected override void Awake()
-    {
-        base.Awake();
-        shootSync = this.GetComponent<EnemyShootSync>();
-        ResetShootCooldown();
+	protected override void Awake ()
+	{
+		base.Awake ();
+		shootSync = this.GetComponent<EnemyShootSync> ();
+		agent = this.GetComponent<NavMeshAgent> ();
+		animController = this.GetComponent<AnimationControllerScript> ();
 
-        Client.OnUserDisconnected += (id) =>
-        {
-            targetToShot = null;
-        };
+		ResetShootCooldown ();
 
-        gun = GetComponentInChildren<Gun>();
-    }
+		Client.OnUserDisconnected += (id) => {
+			targetToShot = null;
+		};
 
-    private void Update()
-    {
-        if (targetToShot != null)
-            this.transform.LookAt(targetToShot.transform.position);
+		gun = GetComponentInChildren<Gun> ();
+	}
 
-        currentCooldownBetweenShoots -= Time.deltaTime;
+	private void Update ()
+	{
+		Move ();
 
-        if (currentCooldownBetweenShoots <= 0)
-            Shot();
-    }
+		if (targetToShot != null)
+			this.transform.LookAt (targetToShot.transform.position);
 
-    private void Shot()
-    {
-        if (targetToShot == null || targetToShot.Dead)
-        {
-            owner.SwitchState(targetLost);
-            return;
-        }
+		currentCooldownBetweenShoots -= Time.deltaTime;
 
-        RaycastHit hit;
-        Vector3 currentPos = transform.position;
-        Vector3 dir = (targetToShot.transform.position - currentPos).normalized;
-        Transform shot = null;
+		if (currentCooldownBetweenShoots <= 0)
+			Shot ();
+	}
+
+	private void Move ()
+	{
+		currentCooldownLeftBeforeRecalculation -= Time.deltaTime;
+		if (targetToShot != null && currentCooldownLeftBeforeRecalculation <= 0) {
+			int index = CheckIfTargetInVision ();
+			if (index == -1)
+				this.agent.isStopped = true;
+			else {
+				this.agent.isStopped = false;
+				if (index == 0)
+					this.agent.SetDestination (targetToShot.transform.position);
+				else {
+					Vector3 destination = (transform.position - targetToShot.transform.position).normalized * midDistance;
+					if (!this.agent.SetDestination (destination)) {
+						if (NavMesh.SamplePosition ((targetToShot.transform.position - transform.position).normalized * (minDistance * 1.5f), out navMeshHit, minDistance * 0.5f, NavMesh.AllAreas))
+							this.agent.SetDestination (navMeshHit.position);
+						else
+							this.agent.isStopped = true;
+					}
+				}
+			}
+		}
+
+		Vector3 vel = this.agent.velocity.normalized;
+		animController.Animation (vel.x, vel.z);
+	}
+
+	private void Shot ()
+	{
+		if (targetToShot == null || targetToShot.Dead) {
+			owner.SwitchState (targetLost);
+			return;
+		}
+
+		RaycastHit hit;
+		Vector3 currentPos = transform.position;
+		Vector3 dir = (targetToShot.transform.position - currentPos).normalized;
+		Transform shot = null;
 
 
-        if (Physics.Raycast(currentPos + new Vector3(0, 0.5f, 0), dir, out hit, maxViewDistance, targetLayerMask))
-            shot = hit.collider.transform.root;
+		if (Physics.Raycast (currentPos + new Vector3 (0, 0.5f, 0), dir, out hit, maxViewDistance, targetLayerMask))
+			shot = hit.collider.transform.root;
 
-        if (!shot || shot != targetToShot.transform)
-        {
-            owner.SwitchState(targetLost);
-            return;
-        }
+		if (!shot || shot != targetToShot.transform) {
+			owner.SwitchState (targetLost);
+			return;
+		}
 
-        targetToShot.DecreaseLife(owner.enemy != null ? ((EnemyStats)owner.enemy.Stats).Damage : 1);
-        transform.LookAt(shot, Vector3.up);
+		targetToShot.DecreaseLife (owner.enemy != null ? ((EnemyStats)owner.enemy.Stats).Damage : 1);
+		transform.LookAt (shot, Vector3.up);
 
-        ResetShootCooldown();
-        if (shootSync != null)
-            shootSync.SendShotCall();
+		ResetShootCooldown ();
+		if (shootSync != null)
+			shootSync.SendShotCall ();
 
-        if (gun != null)
-            gun.Shoot();
-    }
+		if (gun != null)
+			gun.Shoot ();
+	}
 
-    public override void OnStateEnter()
-    {
-        AIGoToTargetUntilOnSight previousState = owner.PreviousState as AIGoToTargetUntilOnSight;
-        if (previousState == null)
-        {
-            owner.SwitchState(targetLost);
-            return;
-        }
+	public override void OnStateEnter ()
+	{
+		AIGoToTargetUntilOnSight previousState = owner.PreviousState as AIGoToTargetUntilOnSight;
+		if (previousState == null) {
+			owner.SwitchState (targetLost);
+			return;
+		}
 
-        targetToShot = previousState.Target.GetComponent<Player>();
-        if (!targetToShot || targetToShot.Dead)
-        {
-            owner.SwitchState(targetLost);
-            return;
-        }
+		targetToShot = previousState.Target.GetComponent<Player> ();
+		if (!targetToShot || targetToShot.Dead) {
+			owner.SwitchState (targetLost);
+			return;
+		}
 
-        ResetShootCooldown();
-    }
+		ResetShootCooldown ();
 
-    private void ResetShootCooldown()
-    {
-        this.currentCooldownBetweenShoots = UnityEngine.Random.Range(minCooldownBetweenShoots, maxCooldownBetweenShoots);
-    }
+		agent.isStopped = false;
+	}
 
-    public override void OnStateExit()
-    {
-        targetToShot = null;
-    }
+	private void ResetShootCooldown ()
+	{
+		this.currentCooldownBetweenShoots = UnityEngine.Random.Range (minCooldownBetweenShoots, maxCooldownBetweenShoots);
+	}
+
+	public override void OnStateExit ()
+	{
+		targetToShot = null;
+		agent.isStopped = true;
+	}
+
+	private int CheckIfTargetInVision ()
+	{
+		if (targetToShot == null) {
+			owner.SwitchState (targetLost);
+			return -1;
+		}
+
+		Vector3 targetPos = targetToShot.transform.position;
+		Vector3 pos = transform.position;
+
+		float distanceToPlayer = Vector3.Distance (pos, targetPos);
+		if (distanceToPlayer > maxViewDistance) {
+			owner.SwitchState (goToPlayer);
+			return -1;
+		}
+
+		Vector3 directionToPlayer = (targetPos - pos).normalized;
+		float angleToPlayer = Vector3.Angle (this.transform.forward, directionToPlayer);
+		if (angleToPlayer > fov * 0.5f)
+			return 0;
+
+		RaycastHit hit;
+
+		Vector3 raycastPositionStart = pos + new Vector3 (0f, 0.5f, 0f);
+
+		if (!targetToShot || !targetToShot.gameObject.activeSelf)
+			owner.SwitchState (targetLost);
+		else if (Physics.Raycast (raycastPositionStart, directionToPlayer, out hit, maxViewDistance, targetLayerMask) && hit.collider.transform.root == targetToShot.transform) {
+			if (distanceToPlayer > midDistance)
+				return 0;
+			else if (distanceToPlayer < minDistance)
+				return 1;
+		}
+
+		return -1;
+	}
 }
